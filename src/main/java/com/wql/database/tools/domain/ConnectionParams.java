e0b1c8ab1b3cd49d39b7a511a756d8ba276eb629
xwq <455018010@qq.com>
2019-06-03 19:13:29 +0800

    添加数据库连接参数验证注解

--- src/main/java/com/wql/database/tools/domain/ConnectionParams.java
+++ src/main/java/com/wql/database/tools/domain/ConnectionParams.java
@@ -2,6 +2,8 @@ package com.wql.database.tools.domain;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 /**
  * 数据库连接参数
  * Created by wendrewshay on 2019/05/31
@@ -11,21 +13,26 @@ public class ConnectionParams {
     /**
      * 数据库连接IP
      */
+    @NotBlank(message = "数据库连接IP不能为空")
     private String ip;
     /**
      * 数据库连接端口
      */
+    @NotBlank(message = "数据库连接端口不能为空")
     private String port;
     /**
      * 数据库连接用户名
      */
+    @NotBlank(message = "数据库连接用户名不能为空")
     private String username;
     /**
      * 数据库连接密码
      */
+    @NotBlank(message = "数据库连接密码不能为空")
     private String password;
     /**
      * 数据库名称
      */
+    @NotBlank(message = "数据库名称不能为空")
     private String dbName;
 }
