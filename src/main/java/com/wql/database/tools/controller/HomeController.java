569b6fef92ba9c1b95ae2355bcf99b438f3bd15f
xwq <455018010@qq.com>
2019-06-03 19:14:13 +0800

    添加@Validated注解，对参数错误结果进行输出验证

--- src/main/java/com/wql/database/tools/controller/HomeController.java
+++ src/main/java/com/wql/database/tools/controller/HomeController.java
@@ -6,12 +6,15 @@ import com.wql.database.tools.service.SchemaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 /**
@@ -32,7 +35,10 @@ public class HomeController {
      */
     @GetMapping(value = "/queryTables")
     @ResponseBody
-    public List queryTables(ConnectionParams params) {
+    public List queryTables(HttpServletResponse response, @Validated ConnectionParams params, BindingResult result) throws IOException {
+        if (handleValidatedError(response, result)) {
+            return null;
+        }
         return schemaService.queryTables(params);
     }
 
@@ -44,12 +50,44 @@ public class HomeController {
      */
     @GetMapping(value = "/queryColumnsOf/{tableName}")
     @ResponseBody
-    public List<ColumnInfo> queryColumnInfo(ConnectionParams params, @PathVariable(name = "tableName") String tableName) {
+    public List<ColumnInfo> queryColumnInfo(HttpServletResponse response, @Validated ConnectionParams params,
+                                            @PathVariable(name = "tableName") String tableName, BindingResult result) throws IOException {
+        if (handleValidatedError(response, result)) {
+            return null;
+        }
         return schemaService.queryColumnInfo(params, tableName);
     }
 
+    /**
+     * 导出指定数据库中所有表字段信息到Excel
+     * @param response 响应对象
+     * @param params   数据库连接参数
+     */
     @GetMapping(value = "export")
-    public void export(HttpServletRequest request, HttpServletResponse response, ConnectionParams params) {
+    public void export(HttpServletResponse response, @Validated ConnectionParams params, BindingResult result) throws IOException {
+        if (handleValidatedError(response, result)) {
+            return;
+        }
+        schemaService.handle(response, params);
+    }
 
+    /**
+     * 处理验证错误信息
+     * @param response 响应对象
+     * @param result   错误结果
+     * @return
+     */
+    private boolean handleValidatedError(HttpServletResponse response, BindingResult result) throws IOException {
+        if (result.hasErrors()) {
+            String errorMessage = result.getAllErrors().get(0).getDefaultMessage();
+            log.error(">>> 参数验证错误：{}", errorMessage);
+            response.setCharacterEncoding("GBK");
+            PrintWriter writer = response.getWriter();
+            writer.write(errorMessage);
+            writer.flush();
+            writer.close();
+            return true;
+        }
+        return false;
     }
 }
